dd9a55590f964102e549f0733a664403ced0c43a
fergmastaflex <joey@fergmedia.com>
2021-02-03 18:35:39 -0500

    Add missing spec and fix failing spec"

--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,11 +1,18 @@
 RSpec.describe AirVisualApi::City do
   subject { described_class.new }
 
-  before { expect_any_instance_of(AirVisualApi::Client).to receive(:get).with({ url: expected_url }) }
+  describe '.nearest_city_by_ip' do
+    let(:expected_url) { '/nearest_city' }
+    it 'sends the ip in the headers' do
+      expect_any_instance_of(AirVisualApi::Client).to receive(:get).with({ url: expected_url, headers: { 'x-forwarded-for' => '123.45.67.890' }})
+      subject.nearest_city_by_ip(ip: '123.45.67.890')
+    end
+  end
 
   describe '.nearest_city_by_gps' do
     let(:expected_url) { '/nearest_city?lat=123.0&lon=456.0' }
     it 'interpolates the URI correctly' do
+      expect_any_instance_of(AirVisualApi::Client).to receive(:get).with({ url: expected_url })
       subject.nearest_city_by_gps(latitude: 123, longitude: 456)
     end
   end
@@ -14,6 +21,7 @@ RSpec.describe AirVisualApi::City do
     let(:expected_url) { '/city?city=Arlington&state=Virginia&country=USA' }
 
     it 'interpolates the URI correctly' do
+      expect_any_instance_of(AirVisualApi::Client).to receive(:get).with({ url: expected_url })
       subject.city_by_name_and_state(city: 'Arlington', state: 'Virginia')
     end
   end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,11 +3,12 @@ RSpec.describe AirVisualApi::Client do
   let(:base_url) { 'http://api.airvisual.com/v2' }
   let(:key_param) { '&key=12345' }
   let(:expected_url) { base_url + '/cities?state=WA' + key_param }
+  let(:headers) { { headers: { "Accept"=>"application/json", "Content-Type"=>"application/json" } } }
 
   subject { described_class.new(api_key: '12345') }
 
   it 'sends get request to API' do
-    expect(HTTParty).to receive(:get).with(expected_url)
+    expect(HTTParty).to receive(:get).with(expected_url, headers)
     subject.get(url: '/cities?state=WA')
   end
 end
